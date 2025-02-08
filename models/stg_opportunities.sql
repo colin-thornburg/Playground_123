6b09bb46dba1306f7c788e493a62b843d5b5bbc8
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2025-02-08 12:26:41 +0000

    changes

--- models/stg_opportunities.sql
+++ models/stg_opportunities.sql
@@ -5,6 +5,6 @@ SELECT
     created_date,
     submitted_date,
     -- Create concat keys for joins
-    CONCAT(created_date, '_', owner_id) as created_date_user_id_concat,
-    CONCAT(submitted_date, '_', owner_id) as submitted_date_user_id_concat
+    CONCAT(created_date::varchar, '_', owner_id) as created_date_user_id_concat,
+    CONCAT(submitted_date::varchar, '_', owner_id) as submitted_date_user_id_concat
 FROM {{ ref('raw_opportunities') }}
--- models/stg_sales_hierarchy.sql
+++ models/stg_sales_hierarchy.sql
@@ -2,6 +2,8 @@ SELECT
     hierarchy_id,
     sfdc_user_id,
     reporting_segment,
+    region_name,
+    area_name,
     start_date,
     end_date
 FROM {{ ref('raw_sales_hierarchy') }}
